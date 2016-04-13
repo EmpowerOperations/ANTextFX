fda2eb4d8412c39c5d038b978c21987434569d5b
groostav-desktop <geoff_groos@msn.com>
2016-04-12 21:57:29 -0700

    added source and dependencies

---
+++ src/src/main/com/empowerops/antextfx/ErrorUnderlineHighlighter.java
@@ -0,0 +1,42 @@
+package com.empowerops.antextfx;
+
+import com.empowerops.antextfx.StructuredHighlighter;
+import com.google.common.collect.ImmutableRangeMap;
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeMap;
+import javafx.beans.NamedArg;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ErrorNode;
+
+public class ErrorUnderlineHighlighter implements StructuredHighlighter.ErrorHighlighter {
+
+    private final String cssStyleClass;
+    private final boolean includeTextUnderCaret;
+
+    //TODO docs
+    // include cursor = should we apply the error style if the cursor is on that element?
+    public ErrorUnderlineHighlighter(@NamedArg(value="styleClass", defaultValue="error") String styleClass,
+                                     @NamedArg(value="includeCaret", defaultValue="false") boolean includeTextUnderCaret) {
+        this.cssStyleClass = styleClass;
+        this.includeTextUnderCaret = includeTextUnderCaret;
+    }
+
+    @Override
+    public RangeMap<Integer, String> generateNewStylesForLexerError(StructuredTextArea parent,
+                                                                    Token problemToken,
+                                                                    String antlrGeneratedMessage,
+                                                                    RecognitionException exception) {
+
+        Range<Integer> errorRange = Range.closed(problemToken.getStartIndex(), problemToken.getStopIndex());
+
+        return ! includeTextUnderCaret && errorRange.contains(parent.getCaretPosition() - 1)
+                ? NO_NEW_HIGHLIGHTS
+                : ImmutableRangeMap.of(errorRange, cssStyleClass);
+    }
+
+    @Override
+    public RangeMap<Integer, String> generateNewStylesForParserError(StructuredTextArea parent, ErrorNode error) {
+        return NO_NEW_HIGHLIGHTS;
+    }
+}
---
+++ src/src/main/com/empowerops/antextfx/StructuredHighlighter.java
@@ -0,0 +1,65 @@
+package com.empowerops.antextfx;
+
+import com.google.common.collect.ImmutableRangeMap;
+import com.google.common.collect.RangeMap;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * Created by Geoff on 4/6/2016.
+ */
+public interface StructuredHighlighter {
+
+    ImmutableRangeMap<Integer, String> NO_NEW_HIGHLIGHTS = ImmutableRangeMap.of(); //empty
+
+    interface TokenHighlighter extends StructuredHighlighter {
+
+        //TODO docs
+        //EOF is at range [-1, -1]
+        RangeMap<Integer, String> generateNewStyles(StructuredTextArea parent, RangeMap<Integer, Token> newTokenStream);
+
+        //TODO currently the bracket implementation of this abuses the caretPosition listener
+        // to force a restyle every time the caret position changes.
+        // No recompile, but that still isn't necessarily cheap.
+        // If i made this interface stateful, I could optimize that, but state is icky.
+        // could do a `boolean needsRestyle(STA parent, int oldPosition, intNewPosition)`,
+        // with a default implementation to return false, but, ehhh,
+    }
+
+    interface ParseRuleHighlighter extends StructuredHighlighter {
+
+        default RangeMap<Integer, String> generateNewStyles(StructuredTextArea parent, ParseTree newParseTree){
+            return NO_NEW_HIGHLIGHTS;
+        }
+
+        default RangeMap<Integer, String> generateNewStyles(StructuredTextArea parent, ParserRuleContext productionOnNewTree){
+            return generateNewStyles(parent, (ParseTree) productionOnNewTree);
+        }
+        default RangeMap<Integer, String> generateNewStyles(StructuredTextArea parent, TerminalNode terminalOnNewTree){
+            return generateNewStyles(parent, (ParseTree) terminalOnNewTree);
+        }
+    }
+
+    interface ErrorHighlighter extends StructuredHighlighter {
+
+        RangeMap<Integer, String> generateNewStylesForLexerError(StructuredTextArea parent,
+                                                                 Token problemToken,
+                                                                 String antlrGeneratedMessage,
+                                                                 RecognitionException exception);
+
+        RangeMap<Integer, String> generateNewStylesForParserError(StructuredTextArea parent, ErrorNode error);
+
+        //TODO docs
+        // this is the case where the document is length zero, or the token is an EOF problem.
+        default RangeMap<Integer, String> generateNewStylesForTokenFailure(StructuredTextArea parent,
+                                                                           Token problemToken,
+                                                                           String antlrGeneratedMessage,
+                                                                           RecognitionException exception){
+            return NO_NEW_HIGHLIGHTS;
+        }
+    }
+}
